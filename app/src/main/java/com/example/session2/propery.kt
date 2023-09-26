41fe76e76a50e22edb276a0a827600d9701ad67b
juandiegows <mejiamaestrejuandiego@gmail.com>
2023-09-26 16:56:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/session2/propery.kt
@@ -0,0 +1,12 @@
+package com.example.session2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class propery : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_propery)
+        supportActionBar!!.title = "Propery Name 1 prices "
+    }
+}
